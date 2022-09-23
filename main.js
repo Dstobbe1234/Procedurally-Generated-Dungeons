2b6dc7b035d85f6f9da8d91ef00856ee55d7fcca
David Stobbe <d.stobbe1@share.epsb.ca>
2022-09-23 10:04:41 -0600

    Sort of started

--- main.js
+++ main.js
@@ -8,24 +8,71 @@ function randomInt(min, max) {
 }
 
 // Random walk - procedural dungeon generation
+
 let dead = false
-async function proceduralCorridors() {
-    const vectorIdentifiers = [[0, -1], [0, 1], [-1, 0], [1, 0]];
-    let previousVector = 0
-    while(!dead) {
-        let randomSegmentLength = randomInt(50, 100);
-        let randomVectorIndex = randomInt(0, vectorIdentifiers.splice(previousVector, 1).length);
-        let Vector = vectorIdentifiers.splice(previousVector, 1)[randomVectorIndex];
-        for(let m = 0; m < randomSegmentLength; m+= 10) {
-            //ctx.fillRect(m * Vector[0], m * Vector[1], 10, 10)
-            //Corridor Tile class
-        }
+corridorTileArr = []
+
+class corridorTile {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
     }
-
-
-
+    fillRect() {
+        if(this.x === 500 && this.y === 500) {
+            ctx.fillStyle = "red"
+        } else {
+            ctx.fillStyle = "black"
+        }
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+    }
 }
 
-function testDead() {
-    dead = true
+
+function proceduralCorridors() {
+    let vectorArr = [[[0, -1], [0, 1]], [[-1, 0], [1, 0]]];
+    let previousVectorIndex = [0, 0]
+    let x = 500;
+    let y = 500;
+    for(let i = 0; i < 20; i++) {
+        vectorArr.splice(previousVectorIndex[0], 1)
+        let randomSegmentLength = randomInt(10, 50);
+        let randomVectorIndex = randomInt(0, 2);
+        let vector = vectorArr[0].concat(vectorArr)[randomVectorIndex];
+
+        for(let m = 0; m < randomSegmentLength; m++) {
+            x += (2 * vector[0]);
+            y += (2 * vector[1]);
+            corridorTileArr.push(new corridorTile(x, y, 2, 2))
+        }
+        console.log(vectorArr)
+        console.log(randomVectorIndex)
+        console.log(vector);
+
+        vectorArr = [[[0, -1], [0, 1]], [[-1, 0], [1, 0]]];
+        for(let p = 0; p < vectorArr; p++) {
+            for(let l = 0; l < vectorArr[p]; l++) {
+                if(vectorArr[p][l][0] === vector[0] && vectorArr[p][l][1] === vector[1]) {
+                    previousVectorIndex = [p, l]
+                    break;
+                }
+            }
+        }
+
+    }
+    for(let test = 0; test < corridorTileArr.length; test++) {
+        setTimeout(function() {
+            displayTiles(corridorTileArr[test])
+        }, 1000)
+    }
+    //displayTiles()
 }
+
+function displayTiles(tile) {
+    tile.fillRect()
+}
+
+
+
+//proceduralCorridors()
