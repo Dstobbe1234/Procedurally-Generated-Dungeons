961c504473ae12fd53836bf859ae3a0ff525669f
David Stobbe <d.stobbe1@share.epsb.ca>
2022-09-23 18:02:24 -0600

    hi

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const cnv = document.getElementById("cnv");
 const ctx = cnv.getContext("2d");
 cnv.width = 1000;
 cnv.height = 1000;
-
+// hi
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
