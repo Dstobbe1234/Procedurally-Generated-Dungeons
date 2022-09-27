e0b96721634085013209cdc5a71b9b6baf78585c
David Stobbe <d.stobbe1@share.epsb.ca>
2022-09-26 19:05:47 -0600

    David

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const cnv = document.getElementById("cnv");
-const ctx = cnv.getContext("2d");
-cnv.width = 1000;
-cnv.height = 1000;
-// hi
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
@@ -10,59 +5,12 @@ function randomInt(min, max) {
 let player = {
   angle: 0,
   draw: function () {
-    if (mouse.down) {
-      let yDist = mouse.y - 520;
-      let xDist = mouse.x - 520;
-      let mouseAngle = (Math.atan(xDist / yDist) * 180) / Math.PI;
-      console.log(mouseAngle);
-
-      ctx.beginPath();
-      ctx.moveTo(520, 520);
-      ctx.lineTo(520, yDist + 520);
-      ctx.lineTo(xDist + 520, yDist + 520);
-      ctx.lineTo(520, 520);
-      ctx.stroke();
-    }
+    ctx.fillStyle = "purple";
+    playerAnim();
   },
 };
 
 let playerDisplacement = [0, 0];
-
-document.addEventListener("keydown", keydownListener);
-document.addEventListener("mousedown", mousedownListener);
-document.addEventListener("mouseup", mouseupListener);
-document.addEventListener("mousemove", mousemoveListener);
-
-function keydownListener(event) {
-  if (event.key === "w") {
-    playerDisplacement[1] += 50;
-  } else if (event.key === "s") {
-    playerDisplacement[1] -= 50;
-  } else if (event.key === "d") {
-    playerDisplacement[0] -= 50;
-  } else if (event.key === "a") {
-    playerDisplacement[0] += 50;
-  }
-}
-
-let mouse = {
-  x: 0,
-  y: 0,
-  down: false,
-};
-
-function mousedownListener() {
-  mouse.down = true;
-}
-
-function mouseupListener() {
-  mouse.down = false;
-}
-
-function mousemoveListener(event) {
-  mouse.x = event.x - cnv.getBoundingClientRect().x;
-  mouse.y = event.y - cnv.getBoundingClientRect().y;
-}
 
 // Random walk - procedural dungeon generation
 
@@ -95,48 +43,50 @@ function generateCorridors() {
   let previousVectorIndex = [0, 0];
   let x = 500;
   let y = 500;
-  let r = 0;
-  for (let i = 0; i < 20; i++) {
-    vectorArr.splice(previousVectorIndex[0], 1);
+  for (let i = 0; i <= 20; i++) {
     let randomSegmentLength = randomInt(10, 50);
-    let randomVectorIndex = randomInt(0, 2);
-    let vector = vectorArr[0][randomVectorIndex];
-    for (let m = 0; m < randomSegmentLength; m++) {
-      x += 100 * vector[0];
-      y += 100 * vector[1];
-      corridorTileArr.push(new corridorTile(x, y, 100, 100));
+    let vector;
+    if (i !== 0 && i !== 20) {
+      vectorArr.splice(previousVectorIndex[0], 1);
+      let randomVectorIndex = randomInt(0, 2);
+      vector = vectorArr[0][randomVectorIndex];
+    } else {
+      vector = [0, -1];
     }
-    r += 100;
-    vectorArr = [
-      [
-        [0, -1],
-        [0, 1],
-      ],
-      [
-        [-1, 0],
-        [1, 0],
-      ],
-    ];
-    for (let p = 0; p < vectorArr.length; p++) {
-      for (let l = 0; l < vectorArr[p].length; l++) {
-        if (vectorArr[p][l][0] === vector[0] && vectorArr[p][l][1] === vector[1]) {
-          previousVectorIndex = [p, l];
-          break;
+    for (let m = 0; m < randomSegmentLength; m++) {
+      x += 2 * vector[0];
+      y += 2 * vector[1];
+      corridorTileArr.push(new corridorTile(x, y, 2, 2));
+      vectorArr = [
+        [
+          [0, -1],
+          [0, 1],
+        ],
+        [
+          [-1, 0],
+          [1, 0],
+        ],
+      ];
+      for (let p = 0; p < vectorArr.length; p++) {
+        for (let l = 0; l < vectorArr[p].length; l++) {
+          if (vectorArr[p][l][0] === vector[0] && vectorArr[p][l][1] === vector[1]) {
+            previousVectorIndex = [p, l];
+            break;
+          }
         }
       }
+      requestAnimationFrame(loop);
     }
+  }
+  function loop() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+    for (let i = 0; i < corridorTileArr.length; i++) {
+      corridorTileArr[i].draw();
+    }
+    player.draw();
     requestAnimationFrame(loop);
   }
-}
-function loop() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, cnv.width, cnv.height);
-  for (let i = 0; i < corridorTileArr.length; i++) {
-    corridorTileArr[i].draw();
-  }
-  player.draw();
-  ctx.fillRect(500, 500, 40, 40);
-  requestAnimationFrame(loop);
 }
 
 generateCorridors();
