deca4ef374edf6beba8591c536ac84ac8d770405
David Stobbe <d.stobbe1@share.epsb.ca>
2022-09-27 19:34:06 -0600

    david

--- main.js
+++ main.js
@@ -5,27 +5,41 @@ function randomInt(min, max) {
 let player = {
   angle: 0,
   draw: function () {
-    ctx.fillStyle = "purple";
-    playerAnim();
+    //playerAnim();
   },
 };
 
 let playerDisplacement = [0, 0];
-
+dungeonNum = 4;
 // Random walk - procedural dungeon generation
 
 corridorTileArr = [];
+dungeonsArr = [];
 
 class corridorTile {
+  constructor(x, y, w, h, vector) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.vector = vector;
+  }
+  draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x + playerDisplacement[0], this.y + playerDisplacement[1], this.w, this.h);
+  }
+}
+
+class dungeon {
   constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
   }
-  draw() {
+  display() {
     ctx.fillStyle = "black";
-    ctx.fillRect(this.x + playerDisplacement[0], this.y + playerDisplacement[1], this.w, this.h);
+    ctx.fillRect(this.x, this.y, this.w, this.h);
   }
 }
 
@@ -56,7 +70,7 @@ function generateCorridors() {
     for (let m = 0; m < randomSegmentLength; m++) {
       x += 2 * vector[0];
       y += 2 * vector[1];
-      corridorTileArr.push(new corridorTile(x, y, 2, 2));
+      corridorTileArr.push(new corridorTile(x, y, 2, 2, vector));
       vectorArr = [
         [
           [0, -1],
@@ -75,15 +89,37 @@ function generateCorridors() {
           }
         }
       }
-      requestAnimationFrame(loop);
+      createDungeons();
     }
   }
+
+  function createDungeons() {
+    dungeonsArr.push(new dungeon(480, 500, 40, 40));
+    let randTileArr = corridorTileArr;
+    randTileArr.splice(0, 1);
+    randTileArr.splice(randTileArr.length - 1, 1);
+    for (let i = 0; i <= dungeonNum; i++) {
+      let randTileIndex = randInt(0, randTileIndex.length - 1);
+      let dungeonDirection;
+      let possibleDirections;
+      if (
+        JSON.stringify(randTileArr[randTileIndex].vector) ===
+        JSON.stringify([0, 1] || JSON.stringify(randTileArr[randTileIndex].vector) === JSON.stringify([0, -1]))
+      ) {
+        //possibleDirections = [];
+        dungeonDirection = possibleeDirections;
+      }
+    }
+    requestAnimationFrame(loop);
+  }
+
   function loop() {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
     for (let i = 0; i < corridorTileArr.length; i++) {
       corridorTileArr[i].draw();
     }
+    dungeonsArr[0].display();
     player.draw();
     requestAnimationFrame(loop);
   }
