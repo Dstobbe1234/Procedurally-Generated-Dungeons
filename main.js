45120d13c6dd5de8c40b16c762f82295c526b463
David Stobbe <d.stobbe1@share.epsb.ca>
2022-10-25 07:28:16 -0600

    Co-authored-by: OilyPotato819 <OilyPotato819@users.noreply.github.com>

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ cnv.height = 1000;
 
 let hallwaysPng = document.getElementById("hallwaysPng");
 
+// 440 x 440
+
 document.addEventListener("mousedown", mousedownListener);
 document.addEventListener("mouseup", mouseupListener);
 document.addEventListener("mousemove", mousemoveListener);
@@ -24,8 +26,8 @@ function keydownListener(event) {
   }
 }
 
-const size = 435;
-const lengthRange = [3, 10];
+const size = [480, 435];
+const lengthRange = [3, 5];
 const segmentNum = 21;
 let allTiles;
 
@@ -80,14 +82,14 @@ class corridorTile {
       this.imageCoords[3],
       this.position[0] + playerDisplacement[0],
       this.position[1] + playerDisplacement[1],
-      size,
-      size
+      size[0],
+      size[1]
     );
   }
 }
 
 let vector, nextVector, currentSegmentLength, nextSegmentLength;
-let currentPos = [500 - 434 / 2, cnv.height / 2];
+let currentPos = [500 - 480 / 2, cnv.height - 434];
 let equal = false;
 function generateCorridors() {
   let previousVectorIndex;
@@ -112,7 +114,9 @@ function generateCorridors() {
       nextVector = [0, 0];
       vector = [0, -1];
     } else {
-      nextVector = vectorArr.splice(previousVectorIndex[0], 1)[0][randomVectorIndex];
+      nextVector = vectorArr.splice(previousVectorIndex[0], 1)[0][
+        randomVectorIndex
+      ];
     }
     nextSegmentLength = randomInt(lengthRange[0], lengthRange[1]);
 
@@ -127,8 +131,8 @@ function generateCorridors() {
     }
 
     for (let m = 0; m < currentSegmentLength; m++) {
-      currentPos[0] += size * vector[0];
-      currentPos[1] += size * vector[1];
+      currentPos[0] += size[0] * vector[0];
+      currentPos[1] += size[1] * vector[1];
       let img = assignImages(m, currentSegmentLength);
       console.log(img);
       if (vector[0] === 0) {
@@ -145,7 +149,9 @@ function generateCorridors() {
     vector = nextVector;
     currentSegmentLength = nextSegmentLength;
   }
-  allTiles = corridorTiles.vertical.flat(1).concat(corridorTiles.horizontal.flat(1));
+  allTiles = corridorTiles.vertical
+    .flat(1)
+    .concat(corridorTiles.horizontal.flat(1));
   fixDuplicates();
 }
 
@@ -155,10 +161,14 @@ function fixCorridors(segmentOrientation, pos, segmentIndex) {
   firstLoop: for (let w = 0; w < segmentOrientation.length; w++) {
     const segmentTile1 = segmentOrientation[w][0].position[pos[0]];
     //vector[pos[0]] is either 1 or -1
-    const segmentLength = size * (vector[pos[0]] * currentSegmentLength);
+    const segmentLength =
+      size[pos[0]] * (vector[pos[0]] * currentSegmentLength);
     let finalPixel = [currentPos[pos[0]] + segmentLength, currentPos[pos[1]]];
 
-    if (finalPixel[0] >= segmentTile1 - 435 && finalPixel[0] <= segmentTile1 + 435) {
+    if (
+      finalPixel[0] >= segmentTile1 - 435 &&
+      finalPixel[0] <= segmentTile1 + 435
+    ) {
       let segmentPos = [];
 
       if (segmentIndex !== segmentNum - 1) {
@@ -167,7 +177,9 @@ function fixCorridors(segmentOrientation, pos, segmentIndex) {
         }
         let nextSegmentPos = [];
         for (let e = 0; e < nextSegmentLength; e++) {
-          nextSegmentPos.push(finalPixel[1] + size * (nextVector[pos[1]] * e));
+          nextSegmentPos.push(
+            finalPixel[1] + size[pos[0]] * (nextVector[pos[1]] * e)
+          );
         }
 
         for (let x = 0; x < nextSegmentPos.length; x++) {
@@ -205,7 +217,9 @@ function fixDuplicates() {
   for (let i = 0; i < testArr.length; i++) {
     let duplicatesOfi = testArr.filter(
       (coord) =>
-        coord[0] === testArr[i][0] && coord[1] === testArr[i][1] && testArr.indexOf(coord) !== i
+        coord[0] === testArr[i][0] &&
+        coord[1] === testArr[i][1] &&
+        testArr.indexOf(coord) !== i
     );
     if (duplicatesOfi.length !== 0) {
       console.log("adjusted");
@@ -225,31 +239,39 @@ function assignImages(index, length) {
   let tileImg;
   if (index !== length - 1) {
     if (vector[0] === 0) {
-      tileImg = [0, 434, 434, 434];
+      tileImg = [0, 435, 480, 435];
     } else {
-      tileImg = [0, 0, 434, 434];
+      tileImg = [0, 0, 480, 435];
     }
   } else {
     if (
-      (JSON.stringify(vector) === "[0,-1]" && JSON.stringify(nextVector) === "[1,0]") ||
-      (JSON.stringify(vector) === "[-1,0]" && JSON.stringify(nextVector) === "[0,1]")
+      (JSON.stringify(vector) === "[0,-1]" &&
+        JSON.stringify(nextVector) === "[1,0]") ||
+      (JSON.stringify(vector) === "[-1,0]" &&
+        JSON.stringify(nextVector) === "[0,1]")
     ) {
-      tileImg = [434, 0, 434, 434];
+      tileImg = [480, 0, 480, 434];
     } else if (
-      (JSON.stringify(vector) === "[1,0]" && JSON.stringify(nextVector) === "[0,1]") ||
-      (JSON.stringify(vector) === "[0,-1]" && JSON.stringify(nextVector) === "[-1,0]")
+      (JSON.stringify(vector) === "[1,0]" &&
+        JSON.stringify(nextVector) === "[0,1]") ||
+      (JSON.stringify(vector) === "[0,-1]" &&
+        JSON.stringify(nextVector) === "[-1,0]")
     ) {
-      tileImg = [869, 0, 434, 434];
+      tileImg = [960, 0, 480, 434];
     } else if (
-      (JSON.stringify(vector) === "[0,1]" && JSON.stringify(nextVector) === "[1,0]") ||
-      (JSON.stringify(vector) === "[-1,0]" && JSON.stringify(nextVector) === "[0,-1]")
+      (JSON.stringify(vector) === "[0,1]" &&
+        JSON.stringify(nextVector) === "[1,0]") ||
+      (JSON.stringify(vector) === "[-1,0]" &&
+        JSON.stringify(nextVector) === "[0,-1]")
     ) {
-      tileImg = [434, 434, 434, 434];
+      tileImg = [480, 434, 480, 434];
     } else if (
-      (JSON.stringify(vector) === "[1,0]" && JSON.stringify(nextVector) === "[0,-1]") ||
-      (JSON.stringify(vector) === "[0,1]" && JSON.stringify(nextVector) === "[-1,0]")
+      (JSON.stringify(vector) === "[1,0]" &&
+        JSON.stringify(nextVector) === "[0,-1]") ||
+      (JSON.stringify(vector) === "[0,1]" &&
+        JSON.stringify(nextVector) === "[-1,0]")
     ) {
-      tileImg = [869, 434, 434, 434];
+      tileImg = [960, 434, 480, 434];
     } else if (JSON.stringify(nextVector) === "[0,0]") {
       tileImg = [0, 435, 435, 435];
     }
@@ -268,7 +290,7 @@ function loadTorches() {
         16,
         26,
         tile.position[0] + 30 + playerDisplacement[0],
-        tile.position[1] + size / 2 + playerDisplacement[1],
+        tile.position[1] + size[1] / 2 + playerDisplacement[1],
         16,
         26
       );
@@ -280,7 +302,7 @@ function loadTorches() {
         16,
         26,
         tile.position[0] + 348 + playerDisplacement[0],
-        tile.position[1] + size / 2 + playerDisplacement[1],
+        tile.position[1] + size[1] / 2 + playerDisplacement[1],
         16,
         26
       );
@@ -304,27 +326,92 @@ function placeLadders() {
 }
 
 function flicker() {
-  // ctx.fillStyle = "rgba(255, 255, 100, 0.2)";
-  // allTiles.forEach((tile) => {
+  // const bezier = { x: 100, y: 100, r: 50 };
+  // const points = getPoints();
+  // let drawPoints = getPoints();
+  // function getPoints() {
+  //   return [
+  //     { x: bezier.x, y: bezier.y - bezier.r },
+  //     {
+  //       x: bezier.x + bezier.r * 0.55339631805,
+  //       y: bezier.y - bezier.r * 0.99868073281,
+  //     },
+  //     {
+  //       x: bezier.x + bezier.r * 0.99868073281,
+  //       y: bezier.y - bezier.r * 0.55339631805,
+  //     },
+  //     { x: bezier.x + bezier.r, y: bezier.y },
+  //     {
+  //       x: bezier.x + bezier.r * 0.99868073281,
+  //       y: bezier.y + bezier.r * 0.55339631805,
+  //     },
+  //     {
+  //       x: bezier.x + bezier.r * 0.55339631805,
+  //       y: bezier.y + bezier.r * 0.99868073281,
+  //     },
+  //     { x: bezier.x, y: bezier.y + bezier.r },
+  //     {
+  //       x: bezier.x - bezier.r * 0.55339631805,
+  //       y: bezier.y + bezier.r * 0.99868073281,
+  //     },
+  //     {
+  //       x: bezier.x - bezier.r * 0.99868073281,
+  //       y: bezier.y + bezier.r * 0.55339631805,
+  //     },
+  //     { x: bezier.x - bezier.r, y: bezier.y },
+  //     {
+  //       x: bezier.x - bezier.r * 0.99868073281,
+  //       y: bezier.y - bezier.r * 0.55339631805,
+  //     },
+  //     {
+  //       x: bezier.x - bezier.r * 0.55339631805,
+  //       y: bezier.y - bezier.r * 0.99868073281,
+  //     },
+  //   ];
+  // }
+  // function moveBezierPoints() {
+  //   drawPoints.forEach((point, index) => {
+  //     point.x = points[index].x + getRandVector();
+  //     point.y = points[index].y + getRandVector();
+  //   });
+  // }
+  // function drawBezierCircle() {
   //   ctx.beginPath();
-  //   ctx.arc(
-  //     tile.position[0] + 30 + playerDisplacement[0],
-  //     tile.position[1] + size / 2 + playerDisplacement[1],
-  //     175,
-  //     0,
-  //     2 * Math.PI
+  //   ctx.moveTo(drawPoints[0].x, drawPoints[0].y);
+  //   ctx.bezierCurveTo(
+  //     drawPoints[1].x,
+  //     drawPoints[1].y,
+  //     drawPoints[2].x,
+  //     drawPoints[2].y,
+  //     drawPoints[3].x,
+  //     drawPoints[3].y
+  //   );
+  //   ctx.bezierCurveTo(
+  //     drawPoints[4].x,
+  //     drawPoints[4].y,
+  //     drawPoints[5].x,
+  //     drawPoints[5].y,
+  //     drawPoints[6].x,
+  //     drawPoints[6].y
+  //   );
+  //   ctx.bezierCurveTo(
+  //     drawPoints[7].x,
+  //     drawPoints[7].y,
+  //     drawPoints[8].x,
+  //     drawPoints[8].y,
+  //     drawPoints[9].x,
+  //     drawPoints[9].y
+  //   );
+  //   ctx.bezierCurveTo(
+  //     drawPoints[10].x,
+  //     drawPoints[10].y,
+  //     drawPoints[11].x,
+  //     drawPoints[11].y,
+  //     drawPoints[0].x,
+  //     drawPoints[0].y
   //   );
   //   ctx.fill();
-  //   ctx.beginPath();
-  //   ctx.arc(
-  //     tile.position[0] + 348 + playerDisplacement[0],
-  //     tile.position[1] + size / 2 + playerDisplacement[1],
-  //     175,
-  //     0,
-  //     2 * Math.PI
-  //   );
-  //   ctx.fill();
-  // });
+  // }
 }
 
 let left = true;
